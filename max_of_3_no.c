231aff9984ff10d25f9a615db1f31dd396ad5901
purushotam kumar <purushotamkumar60616@gmail.com>
2025-06-21 12:34:22 +0530

    Add files via upload

---
+++ max_of_3_no.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+  int a,b,c;
+  printf("enter three integer");
+  scanf("%d %d %d",&a,&b,&c);
+  if(a>b){
+    if(a>c){
+      printf(" the largest number is: %d\n",a);
+    }else{
+    printf("the largest number is: %d",c);
+  }
+}else{
+    if(b>c){
+     printf("%d",b);
+    }else{
+      printf("%d",c);
+    }
+  }
+    return 0;
+}
+
